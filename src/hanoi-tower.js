ebe0414be43d928e7f4bd9ddc295bae979251a9e
academeg1 <m-ignatovich@hotmail.com>
2022-10-14 11:43:01 +0300

    feat: what season 41/42, done hanoi-tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,9 +14,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function calculateHanoi(disksNumber, turnsSpeed ) {
+  let turn = 0
+  let obj = {
+    turns : turn = (Math.pow(2, disksNumber) - 1),
+    seconds : Math.trunc(turn / (turnsSpeed/3600)),
+  }
+  return obj
 }
 
 module.exports = {
