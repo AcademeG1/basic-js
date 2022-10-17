c8e6100792cd28a1ce4e91ddd636761c81c2d40a
academeg1 <m-ignatovich@hotmail.com>
2022-10-17 14:56:29 +0300

    feat: done encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,12 +10,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(str) {
+function encodeLine(string) {
   let result = ''
-  for (let count = 0; count <= str.length;) {
-
-  }
-  return result
+  let counter = 1
+  string = string.split('')
+  string.forEach((item, index) => {
+    if (string[index] === string[index+1]) {
+      counter++
+    } else {
+      result += counter + string[index]
+      counter = 1
+    }
+  })
+  return result.replace(/1/g, '')
 }
 
 module.exports = {
